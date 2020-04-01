fecd4b7845f10fd90778e7319f86a9d3d2ffb481
Zach Parks <zach@alliware.com>
2020-03-31 18:29:26 -0700

    implemented item block componenet logic

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    extends: [
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    parserOptions: {
+        ecmaVersion: 2019,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    rules: {
+
+    },
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+};
---
+++ src/components/ItemBlock.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "../stylesheets/ItemBlock.scss";
+
+interface Props {
+    /** CSS-Compliant name of the item to be rendered as the background image for this component. */
+    itemName: string;
+    /** Optional: The current count on the quantity on hand and the maximum allowed to carry for this item. */
+    count?: {
+        /** The current quantity on hand. */
+        current: number;
+        /** The maximum quantity for this item. */
+        max: number;
+    };
+}
+
+function ItemBlock({itemName, count}: Props): React.ReactElement {
+    let countClassModifier = "";
+    if (count) {
+        if (count.current === 0) countClassModifier = "item-block__count--empty";
+        if (count.current === count.max) countClassModifier = "item-block__count--full";
+    }
+
+    return (
+        <div className={"item-block " + itemName}>
+            {count && <div className={"item-block__count " + countClassModifier}>{count.current}</div>}
+        </div>
+    );
+}
+
+export default ItemBlock;
