225dfad60f0226e72719604509c51942c2fa0171
Zach Parks <zach@alliware.com>
2020-04-02 12:47:40 -0700

    added watchers to lua script

--- lua/data-emitter.lua
+++ lua/data-emitter.lua
@@ -1,11 +1,13 @@
 local http          = require("socket.http")
 local ltn12         = require("ltn12")
-local eventlistener = require("data-eventlistener")
 
 -- Set the timeout for waiting for a connection to the server.
 -- WARNING: Setting this too high noticiably freeze the emulator until it times out if it loses connection.
 --          Setting this too low may prevent the data from uploading to the server as it'd time out before connecting.
 http.TIMEOUT = 0.1
+
+local sram = {}
+local trigger = false
 
 -- Function for sending the HTTP POST request.
 function send_data(data)
@@ -36,13 +38,75 @@ function jsonify_save_data(data)
     json = json:sub(1, #json - 1)
     return '{' .. json .. '}'
 end
+-- Function for generating ram watchers.
+function create_ram_watcher(variable, address, read_func)
+    return function()
+        local value = read_func(address)
+
+        if (value ~= sram[variable]) then
+            sram[variable] = value
+            trigger = true
+        end
+    end
+end
+function create_ram_watcher_specific(variable, address, watch_val, read_func)
+    -- Initialize our variable if it's not already.
+    if (sram[variable] == nil) then
+        sram[variable] = 0
+    end
+
+    return function()
+        local value = read_func(address)
+
+        console.clear()
+        print(sram[variable])
+        print(watch_val .. ": " .. value)
+
+        if (sram[variable] ~= watch_val and value == watch_val) then
+            sram[variable] = watch_val
+            trigger = true
+        elseif (sram[variable] == watch_val and value ~= watch_val) then
+            -- Reset everything since the state has finished!
+            sram[variable] = 0
+            trigger = true
+        end
+    end
+end
+-- Function for checking for memory updates.
+function event_update()
+    -- Check and update all data variables being watched.
+    --create_ram_watcher('x'           , 0x1DAA54, mainmemory.read_u32_be)()
+    --create_ram_watcher('y'           , 0x1DAA58, mainmemory.read_u32_be)()
+    --create_ram_watcher('z'           , 0x1DAA5C, mainmemory.read_u32_be)()
+    create_ram_watcher_specific('linkState', 0x1DB09C, 0x04000000, mainmemory.read_u32_be)()
+    create_ram_watcher('health'      , 0x11A600, mainmemory.read_u16_be)()
+    create_ram_watcher('maxHealth'   , 0x11A5FE, mainmemory.read_u16_be)()
+    create_ram_watcher('rupees'      , 0x11A604, mainmemory.read_u16_be)()
+    create_ram_watcher('skulltulas'  , 0x11A6A0, mainmemory.read_u16_be)()
+    create_ram_watcher('deaths'      , 0x11A5F2, mainmemory.read_u16_be)()
+    create_ram_watcher('triforces'   , 0x11AE96, mainmemory.read_u16_be)()
+    create_ram_watcher('clothing'    , 0x11A66C, mainmemory.read_u8)()
+    create_ram_watcher('arms'        , 0x11A66D, mainmemory.read_u8)()
+    create_ram_watcher('dekuCapacity', 0x11A671, mainmemory.read_u8)()
+    create_ram_watcher('bSWCapacity' , 0x11A672, mainmemory.read_u8)()
+    create_ram_watcher('qBSCapacity' , 0x11A673, mainmemory.read_u8)()
+    create_ram_watcher('magic'       , 0x11A603, mainmemory.read_u8)()
+    create_ram_watcher('maxMagic'    , 0x11A602, mainmemory.read_u8)()
+    create_ram_watcher('heartPieces' , 0x11A674, mainmemory.read_u8)()
+    create_ram_watcher('statusByte1' , 0x11A675, mainmemory.read_u8)()
+    create_ram_watcher('statusByte2' , 0x11A676, mainmemory.read_u8)()
+    create_ram_watcher('statusByte3' , 0x11A677, mainmemory.read_u8)()
+
+    -- If any of these variables have changed, call for an update.
+    if trigger then
+        trigger = false
+        return true
+    end
+end
 
 while true do
-    local trigger, data = eventlistener.watch()
-
-    if trigger then
-        local json = jsonify_save_data(data)
-        send_data(json)
+    if event_update() then
+        send_data(jsonify_save_data(sram))
     end
 
     emu.frameadvance()
--- server/index.js
+++ server/index.js
@@ -3,14 +3,228 @@ const express = require("express");
 const app     = express();
 const port    = process.env.PORT || 8080;
 
-const print   = require("./lib/print");
-
 app.use(express.json());
 
 app.post("/data", (req, res) => {
-    console.log(req.body);
+    printFancyConsole(req);
 
     res.send();
 });
 
 app.listen(port, () => console.log("Listening on port *:8080"));
+
+function ieee32ToFloat(intval) {
+    var fval = 0.0;
+    var x;//exponent
+    var m;//mantissa
+    var s;//sign
+    s = (intval & 0x80000000)?-1:1;
+    x = ((intval >> 23) & 0xFF);
+    m = (intval & 0x7FFFFF);
+    switch(x) {
+        case 0:
+            //zero, do nothing, ignore negative zero and subnormals
+            break;
+        case 0xFF:
+            if (m) fval = NaN;
+            else if (s > 0) fval = Number.POSITIVE_INFINITY;
+            else fval = Number.NEGATIVE_INFINITY;
+            break;
+        default:
+            x -= 127;
+            m += 0x800000;
+            fval = s * (m / 8388608.0) * Math.pow(2, x);
+            break;
+    }
+    return fval;
+}
+function printFancyConsole(req) {
+    const chalk = require("chalk");
+
+    console.clear();
+    let details = [];
+
+    console.log(chalk.bgWhite.black(
+        // 1234567890123456 12345678 --->
+        "Raw Data Block   Hex      Converted Value                               "
+    ));
+
+    for (let value in req.body) {
+        switch(value) {
+            case "magic":
+                details[value] = ((req.body[value] / 96) * 100) + "%";
+                break;
+            case "maxMagic":
+                details[value] = req.body[value] ? "Level " + req.body[value] : "No Magic" ;
+                break;
+            case "linkState":
+                if (req.body[value] === 0x4000000) {
+                    details[value] = "YOU ARE A FOOL!";
+                } else {
+                    details[value] = "Not A Fool";
+                }
+
+                break;
+            case "maxHealth":
+                details[value] = "♥ " + (req.body[value] / 0x10);
+                break;
+            case "health":
+                details[value] = "♥ " + (Math.floor(req.body[value] / 0x4) / 4);
+                break;
+            case "rupees":
+                details[value] = "♦ " + req.body[value];
+                break;
+
+            case "statusByte1":
+                const v = req.body[value];
+                const string = "[" +
+                    ((v & 0x1)  ? chalk.bgWhite.black("♪Ti") : "♪Ti") + " " +
+                    ((v & 0x2)  ? chalk.bgWhite.black("♪St") : "♪St") + " " +
+                    ((v & 0x4)  ? chalk.bgGreen.black("♣KE") : "♣KE") + " " +
+                    ((v & 0x8)  ? chalk.bgRed.black("♣GR") : "♣GR") + " " +
+                    ((v & 0x10) ? chalk.bgBlue.black("♣ZS") : "♣ZS") + " " +
+                    ((v & 0x20) ? chalk.bgGray.black("◘SoA") : "◘SoA") + " " +
+                    ((v & 0x40) ? chalk.bgGray.black("◘GC") : "◘GC") +
+                    "]";
+
+                details[value] = string;
+                break;
+            case "statusByte2":
+                const w = req.body[value];
+                const string2 = "[" +
+                    ((w & 0x1)  ? chalk.bgBlue.black("♪Se") : "♪Se") + " " +
+                    ((w & 0x2)  ? chalk.bgYellow.black("♪Re") : "♪Re") + " " +
+                    ((w & 0x4)  ? chalk.bgMagenta.black("♪No") : "♪No") + " " +
+                    ((w & 0x8)  ? chalk.bgCyan.black("♪Pr") : "♪Pr") + " " +
+                    ((w & 0x10) ? chalk.bgWhite.black("♪Ze") : "♪Ze") + " " +
+                    ((w & 0x20) ? chalk.bgWhite.black("♪Ep") : "♪Ep") + " " +
+                    ((w & 0x40) ? chalk.bgWhite.black("♪Sa") : "♪Sa") + " " +
+                    ((w & 0x80) ? chalk.bgWhite.black("♪Su") : "♪Su") +
+                    "]";
+
+                details[value] = string2;
+                break;
+            case "statusByte3":
+                const x = req.body[value];
+                const string3 = "[" +
+                    ((x & 0x1)  ? chalk.bgGreen.black("○Fo") : "○Fo") + " " +
+                    ((x & 0x2)  ? chalk.bgRed.black("○Fi") : "○Fi") + " " +
+                    ((x & 0x4)  ? chalk.bgBlue.black("○Wa") : "○Wa") + " " +
+                    ((x & 0x8)  ? chalk.bgYellow.black("○Sp") : "○Sp") + " " +
+                    ((x & 0x10) ? chalk.bgMagenta.black("○Sh") : "○Sh") + " " +
+                    ((x & 0x20) ? chalk.bgCyan.black("○Li") : "○Li") + " " +
+                    ((x & 0x40) ? chalk.bgGreen.black("♪Mi") : "♪Mi") + " " +
+                    ((x & 0x80) ? chalk.bgRed.black("♪Bo") : "♪Bo") +
+                    "]";
+
+                details[value] = string3;
+                break;
+            case "arms":
+                const a = req.body[value];
+                const string4 = "[" +
+                    ((a & 0x1)  ? chalk.bgWhite.black("KoSw") : "KoSw") + " " +
+                    ((a & 0x2)  ? chalk.bgWhite.black("MaSw") : "MaSw") + " " +
+                    ((a & 0x4)  ? chalk.bgWhite.black("BiSw") : "BiSw") + " " +
+                    ((a & 0x10) ? chalk.bgWhite.black("KoSh") : "KoSh") + " " +
+                    ((a & 0x20) ? chalk.bgWhite.black("HySh") : "HySh") + " " +
+                    ((a & 0x40) ? chalk.bgWhite.black("MiSh") : "MiSh") +
+                    "]";
+
+                details[value] = string4;
+                break;
+            case "clothing":
+                const c = req.body[value];
+                const string5 = "[" +
+                    ((c & 0x1)  ? chalk.bgWhite.black("KoTu") : "KoTu") + " " +
+                    ((c & 0x2)  ? chalk.bgWhite.black("GoTu") : "GoTu") + " " +
+                    ((c & 0x4)  ? chalk.bgWhite.black("ZoTu") : "ZoTu") + " " +
+                    ((c & 0x10) ? chalk.bgWhite.black("KoBt") : "KoBt") + " " +
+                    ((c & 0x20) ? chalk.bgWhite.black("IrBt") : "IrBt") + " " +
+                    ((c & 0x40) ? chalk.bgWhite.black("HvBt") : "HvBt") +
+                    "]";
+
+                details[value] = string5;
+                break;
+            case "dekuCapacity":
+                const d = req.body[value];
+
+                let dekuSticks, dekuNuts;
+
+                if      (d & 0x8) { dekuSticks = "Stick Level 3"; }
+                else if (d & 0x4) { dekuSticks = "Stick Level 2"; }
+                else if (d & 0x2) { dekuSticks = "Stick Level 1"; }
+                else              { dekuSticks = "No Sticks"; }
+
+                if      (d & 0x20 && d & 0x10) { dekuNuts = "Nut Level 3"; }
+                else if (d & 0x20) { dekuNuts = "Nut Level 2"; }
+                else if (d & 0x10) { dekuNuts = "Nut Level 1"; }
+                else               { dekuNuts = "No Nuts"; }
+
+                details[value] = "[ " + dekuSticks + ", " + dekuNuts + " ]";
+                break;
+            case "bSWCapacity":
+                const e = req.body[value];
+
+                let scale, wallet, bullets;
+
+                if      (e & 0x4) { scale = "Golden Scale"; }
+                else if (e & 0x2) { scale = "Silver Scale"; }
+                else              { scale = "Can't Swim!!"; }
+
+                if      (e & 0x20 && e & 0x10) { wallet = "Tycoon's Wallet"; }
+                else if (e & 0x20) { wallet = "Giant's Wallet"; }
+                else if (e & 0x10) { wallet = "Adult's Wallet"; }
+                else               { wallet = "Child's Wallet"; }
+
+                if      (e & 0x80 && e & 0x40) { bullets = "Bullet Level 3"; }
+                else if (e & 0x80) { bullets = "Bullet Level 2"; }
+                else if (e & 0x40) { bullets = "Bullet Level 1"; }
+                else               { bullets = "No Bullets"; }
+
+                details[value] = "[ " + bullets + ", " + scale + ", " + wallet + " ]";
+                break;
+            case "qBSCapacity":
+                const f = req.body[value];
+
+                let quiver, bombs, lift;
+
+                if      (f & 0x2 && f & 0x1) { quiver = "Quiver Level 3"; }
+                else if (f & 0x2) { quiver = "Quiver Level 2"; }
+                else if (f & 0x1) { quiver = "Quiver Level 1"; }
+                else              { quiver = "No Quiver"; }
+
+                if      (f & 0x10 && f & 0x8) { bombs = "Bombs Level 3"; }
+                else if (f & 0x10) { bombs = "Bombs Level 2"; }
+                else if (f & 0x8) { bombs = "Bombs Level 1"; }
+                else               { bombs = "No Bombs"; }
+
+                if      (f & 0x80 && f & 0x40) { lift = "Golden Gauntlets"; }
+                else if (f & 0x80) { lift = "Silver Gauntlets"; }
+                else if (f & 0x40) { lift = "Goron's Bracelet"; }
+                else               { lift = "Weak Baby Arms"; }
+
+                details[value] = "[ " + quiver + ", " + bombs + ", " + lift + " ]";
+                break;
+            case "deaths":
+                details[value] = req.body[value] + " Deaths";
+                break;
+            case "skulltulas":
+                details[value] = req.body[value] + " Skulltula Tokens";
+                break;
+            case "heartPieces":
+                details[value] = req.body[value] / 16 + " Heart Pieces";
+                break;
+            case "x":
+            case "y":
+            case "z":
+                details[value] = Math.round(ieee32ToFloat(req.body[value]));
+                break;
+        }
+
+        console.log(
+            value.padEnd(16),
+            (("........" + req.body[value].toString(16).toUpperCase()).slice(-8)),
+            details[value] !== undefined ? details[value] : req.body[value]
+        );
+    }
+}
